043994f8337a11089d3ed36032867aaf85bdee4d
rockstr99 <arpit150100@gmail.com>
2025-01-01 13:36:12 +0530

    added new feature to add new expense by selecting source -committing remaining files

--- src/main/java/com/rockstr99/budget_tracker/controller/ExpensesMasterController.java
+++ src/main/java/com/rockstr99/budget_tracker/controller/ExpensesMasterController.java
@@ -1,16 +1,46 @@
 package com.rockstr99.budget_tracker.controller;
 
+import com.rockstr99.budget_tracker.dao.ExpenseDao;
+import com.rockstr99.budget_tracker.dao.SourceDao;
 import com.rockstr99.budget_tracker.entity.Expense;
+import com.rockstr99.budget_tracker.entity.Source;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/expenses")
 public class ExpensesMasterController {
 
-    public String addNewExpenses(Model model){
+    @Autowired
+    SourceDao sourceDao;
+
+    @Autowired
+    ExpenseDao expenseDao;
+
+    UnaryOperator<String> getJspName = jspName -> "expenseJSPs/" + jspName;
+
+    @GetMapping("/addNewExpenses")
+    public String addNewExpenses(Model model) {
+        List<Source> sources = (List<Source>) sourceDao.getAllSources();
+        List<String> availableSources = sources.stream().map(source -> source.getSourceCode()).collect(Collectors.toList());
         model.addAttribute("expense", new Expense());
-        return "";
+        model.addAttribute("sources", availableSources);
+        return getJspName.apply("addNewExpense");
+    }
+
+    @PostMapping("/submitNewExpense")
+    public String submitNewExpense(@ModelAttribute Expense expense, Model model){
+        expenseDao.addNewExpense(expense);
+        model.addAttribute("expenses", expenseDao.getAllExpenses());
+        return getJspName.apply("getAllExpenses");
     }
 }
--- src/main/webapp/WEB-INF/jsp/expenseJSPs/addNewExpense.jsp
+++ src/main/webapp/WEB-INF/jsp/expenseJSPs/addNewExpense.jsp
@@ -1,12 +1,19 @@
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
 <html>
 <head>
     <title>Result</title>
 </head>
 <body>
     <h2>Form Data</h2>
-    Expense Description:  ${expense.expenseDescription}<br/>
-    Expense Amount:  ${expense.expenseAmount}<br/>
-    Expense Source:  ${expense.source}<br/>
-    <a href="/">Back</a>
+     <form:form method="post" action="/expenses/submitNewExpense" modelAttribute="expense">
+            Expense Description: <form:input path="expenseDescription" /><br/>
+              Expense Amount:  <form:input path="expenseAmount" /><br/>
+            Expense Source:
+              <form:select path="source">
+                                    <form:options items="${sources}" />
+                                </form:select><br/>
+            <input type="submit" value="Submit"/>
+        </form:form>
+
 </body>
 </html>
